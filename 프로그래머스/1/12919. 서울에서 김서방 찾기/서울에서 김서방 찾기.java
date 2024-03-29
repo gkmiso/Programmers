523a0d716e8448cd439d4e7d1cc78cfd2246d0bc
gwak miso <154407635+gkmiso@users.noreply.github.com>
2024-03-29 15:02:17 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 9.06 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String[] seoul) {       
+        int i = 0;
+        for (String s : seoul) {
+            if (s.equals("Kim")) {
+                break;
+            }
+            i++;
+        }
+        
+        return "김서방은 " + i + "에 있다";
+    }
+}
